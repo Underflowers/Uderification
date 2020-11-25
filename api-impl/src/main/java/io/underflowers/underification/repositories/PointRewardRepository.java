4c8cd700fc98bd2d5776c52be126611990f42e06
Thibaud Franchetti <5482364-franchetti@users.noreply.gitlab.com>
2020-11-25 19:44:09 +0100

    Fix inner join

--- api-impl/src/main/java/io/underflowers/underification/repositories/PointRewardRepository.java
+++ api-impl/src/main/java/io/underflowers/underification/repositories/PointRewardRepository.java
@@ -12,7 +12,7 @@ public interface PointRewardRepository extends CrudRepository<PointRewardEntity,
     @Query (value = "SELECT U.app_user_id AS userId, SUM(R.points) AS score\n" +
             "FROM point_reward_entity AS R\n" +
             "INNER JOIN user_entity as U ON R.user_id = U.id\n" +
-            "INNER JOIN point_scale_entity AS S\n" +
+            "INNER JOIN point_scale_entity AS S ON R.point_scale_id = S.id\n" +
             "WHERE S.name=:scale AND S.application_id=:appId\n" +
             "GROUP BY U.app_user_id\n" +
             "ORDER BY score DESC\n" +

e193bb8d8aac635962cfd82d1e868f13da058b93
Doran Kayoumi <dorankayoumi@gmail.com>
2020-11-26 11:33:22 +0100

    Implement retrieval of user badges

--- api-impl/src/main/java/io/underflowers/underification/repositories/BadgeRewardRepository.java
+++ api-impl/src/main/java/io/underflowers/underification/repositories/BadgeRewardRepository.java
@@ -1,7 +1,13 @@
 package io.underflowers.underification.repositories;
 
+import io.underflowers.underification.entities.ApplicationEntity;
+import io.underflowers.underification.entities.BadgeEntity;
 import io.underflowers.underification.entities.BadgeRewardEntity;
+import io.underflowers.underification.entities.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BadgeRewardRepository extends CrudRepository<BadgeRewardEntity, Long> {
+    List<BadgeRewardEntity> findAllByUser(UserEntity user);
 }
